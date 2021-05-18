56886c1164eae65b3b6a7a6d3a8c0cc78efc1bc9
Nandsgit <70517841+Nandsgit@users.noreply.github.com>
2021-05-18 22:22:23 +0545

    Add files via upload

---
+++ src/components/titles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Titles = () =>  (
+    <div>
+        <h1 className = "title-container__title">Weather App </h1>
+        <p className = "title-container__subtitle"> Helps you find weather by city</p>
+    </div>
+      
+);
+export default Titles;
